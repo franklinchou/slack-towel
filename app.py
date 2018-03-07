ad577c2e894e456a438850990fbe1a9338155820
franklin <franklin.chou@yahoo.com>
2018-03-06 20:28:14 -0500

    jsonify response

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, Response
+import os
+from flask import Flask, Response, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def main():
-    return Response("You're a towel.")
+
+    token = request.form.get('token', None)
+
+    if token == os.environ['VERIFICATION_TOKEN']:
+        data = jsonify(text="You're a towel.",
+                       response_type="in_channel")
+
+        r = Response(data, status=200, mimetype='application/json')
+        return r
+    else:
+        return Response("", status=401)
 
 if __name__ == "__main__":
     app.run()
